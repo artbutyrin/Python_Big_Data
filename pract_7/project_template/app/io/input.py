322a7eefdf1c1fdd24aa80b67a3d38a09b6ec0ab
Artem Butyrin <artbutyrin@gmail.com>
2026-03-04 23:14:22 +0200

    add some psevdo code

---
+++ pract_7/project_template/app/io/input.py
@@ -0,0 +1,31 @@
+import pandas as pd
+
+def read_from_console():
+    """
+    Reads text input from the console
+
+    Returns:   str: The text entered by the user
+    """
+    pass
+
+
+def read_from_file():
+    """
+    Reads text from a file using Python built-in capabilities
+
+    Args:   filepath (str): Path to the file to read
+
+    Returns:    str: The content of the file
+    """
+    pass
+
+
+def read_from_file_pandas():
+    """
+    Reads data from a file using the pandas library
+
+    Args:    (str): Path to the file to read.
+
+    Returns:    DataFrame: The content of the file as a pandas DataFrame
+    """
+    pass
---
+++ pract_7/project_template/app/io/output.py
@@ -0,0 +1,18 @@
+def print_to_console():
+    """
+    Prints text to the console
+
+    Args:   text (str): The text to print
+    """
+    pass
+
+
+def write_to_file():
+    """
+    Writes text to a file using Python's built-in capabilities
+
+    Args:
+        text (str): The text to write
+        filepath (str): Path to the file to write to
+    """
+    pass
